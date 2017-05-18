76d589a6515860d1c22a5bcb004080cb346c43dc
a.eirich <a.eirich@flyeralarm.de>
2017-05-18 14:45:06 +0200

    AUTOPAT-1978 implemented user bean

---
+++ IdeaProjects/AutoPatternSearchService/src/main/java/com/flyeralarm/tools/apss/User.java
@@ -0,0 +1,27 @@
+package com.flyeralarm.tools.apss;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class User {
+    private String userName;
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(final String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+}
